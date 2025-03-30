114f6b3e0028ea290299b1472804ea443e035aa5
elvedev <vanyatury2@gmail.com>
2025-03-30 17:25:50 +0200

    feat: add Install-App function

--- script.ps1
+++ script.ps1
@@ -1,36 +1,36 @@
-$LocalTempDir = $env:TEMP;
-$testfolder = 'D:\testfolder';
+Import-Module BitsTransfer;
+
+function Install-App {
+    param (
+        $Link,
+        $FileName
+    )
+
+    $LocalTempDir = $env:TEMP;
+    Write-Host "Start downloading $FileName...";
+    Start-BitsTransfer -Source $Link -Destination "$LocalTempDir/$FileName";
+    if ($FileName -like "*.msi"){
+        $ExitCode = (Start-Process msiexec.exe -Wait -Verb RunAs -ArgumentList "/q /i $LocalTempDir\$FileName" -PassThru).ExitCode;
+    }else{
+        Start-Process -Wait -FilePath "$LocalTempDir\$FileName";
+        $ExitCode = 0;
+    }
+
+    if (0 -eq $ExitCode) {
+        & Write-Host "$FileName is downloaded :)";
+    }
+    else{
+        & Write-Host "Error (ExitCode): $ExitCode";
+    }
+
+    & Remove-Item -LiteralPath "$LocalTempDir\$FileName";
+}
+
+
 $ChromeLink = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi';
 $OfficeLink = 'https://go.microsoft.com/fwlink/?linkid=2264705&clcid=0x409&culture=en-us&country=us';
 $ChromeFileName = 'ChromeInstaller.msi';
 $OfficeFileName = 'OfficeInstaller.exe';
-Write-Host 'Start downloading Google Chrome...';
 
-Import-Module BitsTransfer;
-# Start-BitsTransfer -Source $ChromeLink -Destination "$testfolder/$ChromeFileName"; 
-# Start-BitsTransfer -Source $OfficeLink -Destination "$testfolder/$OfficeFileName";
-# $ExitCode = (Start-Process msiexec.exe -Wait -Verb RunAs -ArgumentList "/q /i $testfolder\$ChromeFileName" -PassThru).ExitCode;
-# if (0 -eq $ExitCode) {
-#     & Write-Host 'Google Chrome is downloaded :)';
-# }else{
-#     & Write-Host "Error (ExitCode): $ExitCode";
-# }
-& "$testfolder\$OfficeFileName" /help;
-
-# or
-# Invoke-WebRequest $url -OutFile $path_to_file
-
-# $ChromeInstaller = "ChromeInstaller.exe"; 
-# (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); 
-# & "$LocalTempDir\$ChromeInstaller" /silent /install;
-# $Process2Monitor =  "ChromeInstaller"; 
-# Do { 
-#         $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; 
-#         If ($ProcessesFound) { 
-#             "Still running: $($ProcessesFound -join ', ')" | Write-Host; 
-#             Start-Sleep -Seconds 2 
-#         } else {
-#             Remove-Item "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose 
-#         } 
-#     } 
-# Until (!$ProcessesFound)
+Install-App $ChromeLink $ChromeFileName;
+Install-App $OfficeLink $OfficeFileName;
